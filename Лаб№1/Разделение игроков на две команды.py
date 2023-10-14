055f35a28c8fdb410fb9038e6de5ed9625dc272c
Rexeton <50441621+Rexeton@users.noreply.github.com>
2023-10-14 23:36:25 +0300

    Add files via upload

---
+++ Лаб№1/Разделение игроков на две команды.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+
+len_list=int(len(list_players)/2)
+#print(len_list)
+print(list_players[:len_list])
+print(list_players[len_list:])
