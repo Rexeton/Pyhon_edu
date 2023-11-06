7ce0dabc194fa84ff3da96acd95ef1790c021cac
Rexeton <50441621+Rexeton@users.noreply.github.com>
2023-11-06 14:06:20 +0300

    Add files via upload

---
+++ Лаб№3/Поиск индекса элемента в списке товаров.py
@@ -0,0 +1,13 @@
+# TODO Напишите функцию для поиска индекса товара
+def f1(items_list,find_item):
+    for idx, el in enumerate(items_list):
+        if el == find_item:
+            return idx
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = f1(items_list,find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
