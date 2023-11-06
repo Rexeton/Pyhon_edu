643de7d7d81345c4cd4ec89ebd26b9dd2dc73fb1
Rexeton <50441621+Rexeton@users.noreply.github.com>
2023-11-06 14:01:25 +0300

    Delete СР№2 directory

--- СР№2/task1.py
+++
@@ -1,7 +0,0 @@
-src = not False and True or False and not True
-
-# TODO расписать упрощение выражения
-
-result = True or False and False  # TODO подставить результат выражения
-
-print(src == result)
--- СР№2/task4.py
+++
@@ -1,6 +0,0 @@
-# TODO завести счетчик для произведения
-# TODO посчитать произведение чисел
-multipl=1
-for i in range(1,11):
-    multipl*=i
-print(multipl)
